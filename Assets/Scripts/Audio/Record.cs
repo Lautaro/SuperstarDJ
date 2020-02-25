cd87bea24f89d0cd63a4b653783421c0e9e038d3
Lautaro <Lautaro.arino@gmail.com>
2020-02-25 16:53:30 +0100

    Added Record class and Prefab

---
+++ Assets/Scripts/Audio/Record.cs
@@ -0,0 +1,14 @@
+﻿using SuperstarDJ.DynamicMusic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperstarDJ.Audio
+{
+    public class Record
+    {
+        Track track;
+    }
+}
