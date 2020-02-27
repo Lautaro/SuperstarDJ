b12c3629d091a83aee1c68e8d2d6ea74147d6c51
Lautaro <Lautaro.arino@gmail.com>
2020-02-27 20:04:31 +0100

    Fixed broken visual metronome

--- Assets/Scripts/VisualMetronome.cs
+++ Assets/Scripts/VisualMetronome.cs
@@ -2,30 +2,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class VisualMetronome : MonoBehaviour
 {
     List<Transform> blips;
     Vector3 blipDefaultSize;
-     RythmPosition rythmPosition;
+    RythmPosition rythmPosition;
+    TextMeshProUGUI text;
+
     // Start is called before the first frame update
     void Start()
     {
+        text = GetComponentInChildren<TextMeshProUGUI> ();
         blips = GetComponentsInChildren<Transform> ().Where ( t => t.name.Length == 1 ).ToList ();
         blipDefaultSize = blips[0].localScale;
         print ( $"Found {blips.Count} transforms" );
-        rythmPosition = MusicManager.RythmPosition ;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ( rythmPosition.Beat != null )
+        text.text = "";
+        if ( MusicManager.RythmPosition.Measure != null )
         {
-            var index = rythmPosition.Beat.index -1;
-            blips.ForEach ( t => t.localScale = blipDefaultSize);
-            blips[index].localScale *= 2;
+            blips.ForEach ( t => t.localScale = blipDefaultSize );
+            rythmPosition = MusicManager.RythmPosition;
+            text.text = $"[{rythmPosition.Measure.index}] - {rythmPosition.Beat.index}";
+            if ( rythmPosition.IsInHitArea )
+            {
+                var index = rythmPosition.Beat.index - 1;
+                blips[index].localScale *= 2;
+              
+            }
         }
     }
 }
